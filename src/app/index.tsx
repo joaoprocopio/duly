dba87e5aaf62bfe9d5ea6abef600f6b898d23678
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-23 18:33:22 -0300

    theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 import { router } from "~/router";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const root = createRoot(document.getElementById("__react") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
