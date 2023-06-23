125a1c9fef5dece2d88a66b7de6d9aa821abe0ae
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-23 12:27:34 -0300

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
-require("@rushstack/eslint-patch/modern-module-resolution")
+/* eslint-disable import/no-extraneous-dependencies */
+require("@rushstack/eslint-patch/modern-module-resolution");
 
-/** @type {import('eslint').Linter.Config} */ 
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: { browser: true, es2020: true, node: true, jest: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
   },
-}
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "18.2",
+    },
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+  },
+};
