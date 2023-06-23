3f6c3506a1c3b0a7c5b33a5812cc5fe182d615bc
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-23 18:56:38 -0300

    tailwindcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,5 @@
+import "~/assets/styles/index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
 export function LandingPage() {
-  return <div>page</div>;
+  return (
+    <h1 className="text-5xl text-blue-500 font-bold underline">Hello world!</h1>
+  );
 }
