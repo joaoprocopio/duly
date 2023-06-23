fea0b81bb33ed3d8cc87fe7110da55fa7acf6613
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-23 19:36:18 -0300

    open

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,
     host: true,
     port: 3000,
   },
   preview: {
+    open: true,
     host: true,
     port: 3001,
   },
