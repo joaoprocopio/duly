4ea25badc4fed22da8e122b209d92553d2141772
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-23 12:22:34 -0300

    add deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,8 @@
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+/** @type {import('eslint').Linter.Config} */ 
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
