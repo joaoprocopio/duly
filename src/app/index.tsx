77ffdffbebe80e66af3a8fd8ec766eae78d58529
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-23 17:20:43 -0300

    clean

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-const root = createRoot(document.getElementById("root") as HTMLElement);
+const root = createRoot(document.getElementById("__react") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <div>hello world!</div>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3001,
+  },
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),
