43215af9e93581de6d358d032c77223e2e82dee9
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-03 19:56:19 -0300

    landing components

---
+++ src/components/landing/LandingIntro/index.tsx
@@ -0,0 +1,25 @@
+import { Typography, Button } from "antd"
+
+const { Title } = Typography
+
+export function LandingIntro() {
+  return (
+    <div className="text-center">
+      <Title className="mx-auto my-8 max-w-3xl text-5xl">
+        Supercharge your React development with Molecule.
+      </Title>
+      <Title
+        level={2}
+        className="mx-auto mb-8 mt-0 max-w-xl text-xl font-medium">
+        A pre-loaded, industry-standard React architecture. Start building what
+        matters without setup hassle.
+      </Title>
+      <div className="mb-64">
+        <Button className="mr-4" type="primary" size="large">
+          Why Molecule?
+        </Button>
+        <Button size="large">Core aspects</Button>
+      </div>
+    </div>
+  )
+}
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,25 +1,9 @@
-import { Typography, Button } from "antd"
-
-const { Title } = Typography
+import { LandingIntro } from "~/components"
 
 export function LandingPage() {
   return (
-    <div className="text-center">
-      <Title className="mx-auto my-8 max-w-3xl text-5xl">
-        Supercharge your React development with Molecule.
-      </Title>
-      <Title
-        level={2}
-        className="mx-auto mb-8 mt-0 max-w-xl text-xl font-medium">
-        A pre-loaded, industry-standard React architecture. Start building what
-        matters without setup hassle.
-      </Title>
-      <div>
-        <Button className="mr-4" type="primary" size="large">
-          Why Molecule?
-        </Button>
-        <Button size="large">Core aspects</Button>
-      </div>
-    </div>
+    <>
+      <LandingIntro />
+    </>
   )
 }
