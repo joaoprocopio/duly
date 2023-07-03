89ffaacbe2776f1f993641e0fba5dba669838c19
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-03 19:45:51 -0300

    landing simpler

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -5,16 +5,14 @@ const { Title } = Typography
 export function LandingPage() {
   return (
     <div className="text-center">
-      <Title className="mx-auto my-8 max-w-5xl text-7xl font-semibold">
+      <Title className="mx-auto my-8 max-w-3xl text-5xl">
         Supercharge your React development with Molecule.
       </Title>
       <Title
         level={2}
-        className="mx-auto mb-12 mt-0 max-w-3xl text-xl font-medium">
-        Molecule comes pre-loaded with a carefully crafted architecture,
-        following industry best practices and standards. It provides a solid
-        foundation for your React applications, allowing you to hit the ground
-        running and concentrate on building features that truly matter.
+        className="mx-auto mb-8 mt-0 max-w-xl text-xl font-medium">
+        A pre-loaded, industry-standard React architecture. Start building what
+        matters without setup hassle.
       </Title>
       <div>
         <Button className="mr-4" type="primary" size="large">
