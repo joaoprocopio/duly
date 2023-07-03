46c45d4dc610426a25726c01d1fa2a24eddb6cdd
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-03 19:17:27 -0300

    landing page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,7 +1,25 @@
-import { Typography } from "antd"
+import { Typography, Button } from "antd"
 
 const { Title } = Typography
 
 export function LandingPage() {
-  return <Title>Welcome to Molecule.</Title>
+  return (
+    <div className="pt-32 text-center">
+      <Title className="mb-8 text-7xl font-semibold">
+        Supercharge your React development with Molecule.
+      </Title>
+      <Title level={2} className="mx-auto mb-16 max-w-3xl text-xl font-medium">
+        Molecule comes pre-loaded with a carefully crafted architecture,
+        following industry best practices and standards. It provides a solid
+        foundation for your React applications, allowing you to hit the ground
+        running and concentrate on building features that truly matter.
+      </Title>
+      <div>
+        <Button className="mr-4" type="primary" size="large">
+          Why Molecule?
+        </Button>
+        <Button size="large">Core aspects</Button>
+      </div>
+    </div>
+  )
 }
