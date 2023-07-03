ce16eccb8de8d4cd53d66794aaa70beaad884132
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-03 13:11:54 -0300

    full error page

--- src/layouts/LandingLayout/index.tsx
+++ src/layouts/LandingLayout/index.tsx
@@ -7,9 +7,7 @@ const { Header, Content } = Layout
 export function LandingLayout() {
   return (
     <>
-      <Header className="sticky top-0" color="transparent">
-        i am a header
-      </Header>
+      <Header className="sticky"></Header>
       <Content>
         <Outlet />
       </Content>
--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom"
 
 import { Typography } from "antd"
+import { ExclamationCircleTwoTone } from "@ant-design/icons"
 
 const { Title, Paragraph, Text } = Typography
 
@@ -8,10 +9,11 @@ export function ErrorPage() {
   const { pathname } = useLocation()
 
   return (
-    <div className="mx-auto h-full w-full max-w-4xl py-8">
+    <div className="mx-auto flex h-full w-full max-w-2xl flex-col items-center gap-16 p-8 sm:flex-row">
+      <ExclamationCircleTwoTone className="text-8xl" />
       <div>
         <Title>Page not found</Title>
-        <Paragraph>
+        <Paragraph className="mb-8">
           Sorry, the page <Text code>{pathname}</Text> that you have requested
           could not be found.
         </Paragraph>
