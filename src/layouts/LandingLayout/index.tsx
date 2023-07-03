79b2d73756de6405f297c8f99c7ae61f8e13f6f5
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-03 13:30:35 -0300

    layout icon

--- src/layouts/LandingLayout/index.tsx
+++ src/layouts/LandingLayout/index.tsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 import { Layout } from "antd"
+
+import { DeploymentUnitOutlined } from "@ant-design/icons"
 
 const { Header, Content } = Layout
 
 export function LandingLayout() {
   return (
     <>
-      <Header className="sticky"></Header>
+      <Header className="sticky p-0">
+        <div className="mx-auto flex h-full max-w-4xl items-center px-8">
+          <Link to="/" className="inline-flex">
+            <DeploymentUnitOutlined className="text-4xl text-white" />
+          </Link>
+        </div>
+      </Header>
       <Content className="mx-auto max-w-4xl p-8">
         <Outlet />
       </Content>
