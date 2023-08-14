8d03b22eaec295d149fb1594ae4f6dd8451f88db
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-14 12:43:55 -0300

    yarn

--- lib/declarations/vitest-env.d.ts
+++ lib/declarations/vitest-env.d.ts
@@ -3,7 +3,5 @@
 import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
 
 declare module "vitest" {
-  interface Assertion<T>
-    extends jest.Matchers<void, T>,
-      TestingLibraryMatchers<T, void> {}
+  interface Assertion<T> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
 }
