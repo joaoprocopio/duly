1ef26e80239bb246ab9ff00c2518e35847ec340e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-30 21:56:07 -0300

    molecule

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,12 @@
 import "@mantine/core/styles.css"
 
+import type { MetaFunction } from "@remix-run/node"
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
 import { MantineProvider, ColorSchemeScript } from "@mantine/core"
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Molecule" }, { name: "description", content: "Welcome to Molecule!" }]
+}
 
 export default function Root() {
   return (
