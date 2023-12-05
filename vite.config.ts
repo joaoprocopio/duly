5ebe9ca32d4752050ddc658f37059e1e55e5317b
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-05 15:33:04 -0300

    config: change remix folder name to src

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [
+    remix({
+      appDirectory: "./src"
+    })
+  ],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url))
     }
-  }
+  },
+  publicDir: "./src/public"
 })
