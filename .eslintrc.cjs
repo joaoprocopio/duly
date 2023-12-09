c135625b61d92550ac01919c83b04368fe404195
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-08 21:55:17 -0300

    melhora as configs do prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
-require("@rushstack/eslint-config/patch/modern-module-resolution")
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["next/core-web-vitals", 
-  "plugin:prettier/recommended"]
+  extends: ["next/core-web-vitals", "plugin:@next/next/recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest"
+  }
 }
