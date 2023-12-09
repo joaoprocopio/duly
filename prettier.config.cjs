cdafc162366bb393214734495b0ef1e55e3455b5
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-08 22:12:16 -0300

    melhorou

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,10 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-  "$schema": "http://json.schemastore.org/prettierrc",
-  "semi": false,
-  "singleQuote": false,
-  "bracketSameLine": true,
-  "vueIndentScriptAndStyle": true,
-  "trailingComma": "none",
-  "printWidth": 100,
-  "quoteProps": "consistent"
+  semi: false,
+  singleQuote: false,
+  bracketSameLine: true,
+  vueIndentScriptAndStyle: true,
+  trailingComma: "none",
+  printWidth: 100,
+  quoteProps: "consistent"
 }
