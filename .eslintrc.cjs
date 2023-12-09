07c189c45d6551ee35ab45a8aeeaee03746cf267
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-08 22:13:18 -0300

    rushstack lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+require("@rushstack/eslint-config/patch/modern-module-resolution")
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["next/core-web-vitals", "plugin:@next/next/recommended", "plugin:prettier/recommended"],
