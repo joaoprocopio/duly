3d018bc817d83c3a93fa7c68ed56f8da2f982c26
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-16 16:16:17 -0300

    landing login

--- app/routes/_landing._index/route.tsx
+++ app/routes/_landing._index/route.tsx
@@ -1,9 +1,5 @@
 import { Container } from "@mantine/core"
 
 export default function LandingRoute() {
-  return (
-    <Container>
-      <p>landing</p>
-    </Container>
-  )
+  return <Container></Container>
 }
--- app/routes/_landing/route.tsx
+++ app/routes/_landing/route.tsx
@@ -1,16 +1,25 @@
-import { ActionIcon, AppShellHeader, AppShellMain, Container } from "@mantine/core"
+import { ActionIcon, AppShellHeader, AppShellMain, Button, Container } from "@mantine/core"
 import { Link, Outlet } from "@remix-run/react"
-import { IconHexagons } from "@tabler/icons-react"
-import styles from "./_landing.module.scss"
+import { IconChevronRight, IconHexagons } from "@tabler/icons-react"
+import "./_landing.scss"
 
 export default function LandingLayout() {
   return (
     <>
-      <AppShellHeader className={styles.header}>
-        <Container className={styles.hContainer} py="0">
-          <ActionIcon component={Link} to="/" size="xl" variant="transparent">
-            <IconHexagons height="100%" width="100%" />
+      <AppShellHeader className="landing-layout-header">
+        <Container className="lyh-container" py="0">
+          <ActionIcon
+            className="lyhc-button"
+            component={Link}
+            size="xl"
+            to="/"
+            variant="transparent">
+            <IconHexagons />
           </ActionIcon>
+
+          <Button component={Link} variant="light" to="/auth" rightSection={<IconChevronRight />}>
+            Entrar
+          </Button>
         </Container>
       </AppShellHeader>
 
