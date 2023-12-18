eedb3f63ada1d3e0b89c33b181595c738b0ef607
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-17 23:49:29 -0300

    start form

--- app/routes/auth._index/route.tsx
+++ app/routes/auth._index/route.tsx
@@ -1,5 +1,20 @@
-import { Container } from "@mantine/core"
+import { Button, Card, Container, TextInput, Title } from "@mantine/core"
+import { Link } from "@remix-run/react"
 
 export default function AuthRoute() {
-  return <Container>auth</Container>
+  return (
+    <Container>
+      <Card withBorder>
+        <Title size="x-large">Seja bem-vindo!</Title>
+
+        <TextInput required withAsterisk={false} label="Usuário ou Email" />
+        <TextInput required withAsterisk={false} label="Senha" />
+
+        <Button type="submit">Entrar</Button>
+        <Button component={Link} to="/auth/registrar" variant="light">
+          Criar conta
+        </Button>
+      </Card>
+    </Container>
+  )
 }
