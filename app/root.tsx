e82bc8a686f6e6d7a884154808c7c939dffb4ca8
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-02-02 21:36:32 -0300

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,11 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap",
   },
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: "/favicon.svg",
+  },
 ]
 
 export default function Root() {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,25 +1,16 @@
 import { Link } from "@remix-run/react"
+import { LucideAtom } from "lucide-react"
 import { Button } from "~/components/ui/button"
 import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
 
 export default function Home() {
   return (
-    <div className="container relative grid h-[100vh] flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative grid h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="bg-muted relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6">
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
+          <LucideAtom className="mr-2 h-6 w-6" />
           Acme Inc
         </div>
         <div className="relative z-20 mt-auto">
